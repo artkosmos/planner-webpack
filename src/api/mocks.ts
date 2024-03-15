76950f3cdfb61307ba77bb6a70fa03258f98f6eb
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2024-03-15 15:51:49 +0300

    change task service logic

---
+++ src/api/mocks.ts
@@ -0,0 +1,9 @@
+import type { ITask } from '@/common/types';
+
+export const mockedTaskList: ITask[] = [
+  { id: '85df17d5', title: 'go camping', date: '2023-03-12T04:46:43' },
+  { id: '9df3077f', title: 'cinema', date: '2021-02-13T15:23:23' },
+  { id: '151dfeb3', title: 'dinner', date: '2019-02-05T02:42:42' },
+  { id: '72e14782', title: 'meeting', date: '2018-02-04T15:06:46' },
+  { id: '7f6d314f', title: 'housework', date: '2023-02-13T02:32:13' },
+];
