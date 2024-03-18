58823824966ac7368bc786b96103483c5648d616
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2024-03-18 11:09:35 +0300

    add library to handle style imports in tests

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   preset: 'ts-jest',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    '^.+\\.(css|scss|png|img)$': 'jest-transform-stub',
   },
   testEnvironment: 'jest-environment-jsdom',
 };
