8e6fd4d34c04334123cbaee78dd50e1e25ad546b
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2024-03-28 12:36:41 +0300

    update translate keys adn i18 config for jest

--- src/__mocks__/i18n-jest.ts
+++ src/__mocks__/i18n-jest.ts
@@ -2,18 +2,26 @@ import { initReactI18next } from 'react-i18next';
 
 import i18n from 'i18next';
 
-import header from '../../public/locales/en/header.json';
-import home from '../../public/locales/en/home.json';
-import task from '../../public/locales/en/task.json';
+import enHeader from '../../public/locales/en/header.json';
+import enHome from '../../public/locales/en/home.json';
+import enTask from '../../public/locales/en/task.json';
+import ruHeader from '../../public/locales/ru/header.json';
+import ruHome from '../../public/locales/ru/home.json';
+import ruTask from '../../public/locales/ru/task.json';
 
 i18n.use(initReactI18next).init({
   lng: 'en',
   fallbackLng: 'en',
   resources: {
     en: {
-      home,
-      task,
-      header,
+      home: enHome,
+      task: enTask,
+      header: enHeader,
+    },
+    ru: {
+      home: ruHome,
+      task: ruTask,
+      header: ruHeader,
     },
   },
 });
