f98588269ecbe9deb2beec1f07e21373cb37201e
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2024-05-18 20:51:28 +0300

    updated styling for dark theme

--- src/components/shared/list-table/list-table.tsx
+++ src/components/shared/list-table/list-table.tsx
@@ -1,7 +1,8 @@
 import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
-// import clsx from 'clsx';
+import clsx from 'clsx';
+
 import { ITask } from '@/common/types';
 import { TableBodyRow } from '@/components/shared/list-table';
 
@@ -18,12 +19,12 @@ type Props = {
 export const ListTable = ({
   list,
   deleteTask,
-  // className,
+  className,
   onRowClick,
   dateFormat,
 }: Props) => {
   const { t } = useTranslation('home');
-  const isMobile = window.matchMedia('(max-width: 768px)').matches;
+  // const isMobile = window.matchMedia('(max-width: 768px)').matches;
 
   const taskList = useMemo(() => {
     return list.map(task => {
@@ -34,29 +35,35 @@ export const ListTable = ({
           key={task.id}
           task={task}
           deleteRow={deleteTask}
-          isMobile={isMobile}
           dateFormat={dateFormat}
         />
       );
     });
   }, [list, dateFormat]);
 
-  // const classNames = {
-  //   container: clsx('table-container', className),
-  //   table: clsx('list-table', 'list-table_dark'),
-  // };
+  const classNames = {
+    container: clsx('table-container', className),
+    table: clsx('table', 'table_dark'),
+    tableHeader: 'table-header',
+    col1: 'col col-1',
+    col2: 'col col-2',
+    col3: 'col col-3',
+    col4: 'col col-4',
+    col5: 'col col-5',
+    tableList: 'table-list',
+  };
 
   return (
-    <div className="table-container">
-      <ul className="table">
-        <li className="table-header">
-          <div className="col col-1">{t('table_column_1')}</div>
-          <div className="col col-2">{t('table_column_2')}</div>
-          <div className="col col-3">{t('table_column_3')}</div>
-          <div className="col col-4"></div>
-          <div className="col col-5"></div>
+    <div className={classNames.container}>
+      <ul className={classNames.table}>
+        <li className={classNames.tableHeader}>
+          <div className={classNames.col1}>{t('table_column_1')}</div>
+          <div className={classNames.col2}>{t('table_column_2')}</div>
+          <div className={classNames.col3}>{t('table_column_3')}</div>
+          <div className={classNames.col4}></div>
+          <div className={classNames.col5}></div>
         </li>
-        <div className={'table-list'}>{taskList}</div>
+        <div className={classNames.tableList}>{taskList}</div>
       </ul>
     </div>
   );
--- src/components/shared/list-table/table-body-row/table-body-row.tsx
+++ src/components/shared/list-table/table-body-row/table-body-row.tsx
@@ -17,7 +17,6 @@ type Props = {
 export const TableBodyRow = ({
   task,
   deleteRow,
-  // isMobile,
   dateFormat,
   ...rest
 }: Props) => {
@@ -32,15 +31,6 @@ export const TableBodyRow = ({
   };
 
   return (
-    // <tr key={task.id} {...rest}>
-    //   {!isMobile && <td>{task.id}</td>}
-    //   <td className={classNames.taskTitle}>{task.title}</td>
-    //   {!isMobile && <td>{dayjs(task.date).format(dateFormat)}</td>}
-    //   <td>{task.important && <StarIcon className={'table-star-icon'} />}</td>
-    //   <td>
-    //     <DeleteIcon className={classNames.delete} onClick={deleteRowHandler} />
-    //   </td>
-    // </tr>
     <li className="table-row" {...rest}>
       <div className="col col-1" data-label="Id">
         {task.id}
