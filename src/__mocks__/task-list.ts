c3c0e9e33e4d3c3ab2ddd7cbeb7a6cde83ec0f7d
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2024-06-17 12:41:55 +0300

    update tests for task-card component

--- src/__mocks__/task-list.ts
+++ src/__mocks__/task-list.ts
@@ -1,39 +1,49 @@
-// import type { ITask } from '@/common/types';
-//
-// export const mockedTaskList: ITask[] = [
-//   {
-//     id: '85df17d5',
-//     title: 'go camping',
-//     date: '2023-03-12T04:46:43',
-//     image: null,
-//     important: false,
-//   },
-//   {
-//     id: '9df3077f',
-//     title: 'cinema',
-//     date: '2021-02-13T15:23:23',
-//     image: null,
-//     important: false,
-//   },
-//   {
-//     id: '151dfeb3',
-//     title: 'dinner',
-//     date: '2019-02-05T02:42:42',
-//     image: null,
-//     important: false,
-//   },
-//   {
-//     id: '72e14782',
-//     title: 'meeting',
-//     date: '2018-02-04T15:06:46',
-//     image: 'img.png',
-//     important: true,
-//   },
-//   {
-//     id: '7f6d314f',
-//     title: 'housework',
-//     date: '2023-02-13T02:32:13',
-//     image: null,
-//     important: true,
-//   },
-// ];
+import type { ITask } from '@/common/types';
+import { TaskStatus } from '@/common/types';
+
+export const mockedTaskList: ITask[] = [
+  {
+    id: '85df17d5',
+    title: 'go camping',
+    date: '2023-03-12T04:46',
+    image: null,
+    important: true,
+    isDone: true,
+    status: TaskStatus.ACTUAL,
+  },
+  {
+    id: '9df3077f',
+    title: 'cinema',
+    date: '2021-02-13T15:23',
+    image: null,
+    important: false,
+    isDone: true,
+  },
+  {
+    id: '151deb35',
+    title: 'dinner',
+    date: '2024-03-13T11:32',
+    image: null,
+    important: false,
+    isDone: false,
+    status: TaskStatus.TODAY,
+  },
+  {
+    id: '72e14782',
+    title: 'meeting',
+    date: '2018-02-04T15:06',
+    image: 'img.png',
+    important: true,
+    isDone: true,
+    status: TaskStatus.DONE,
+  },
+  {
+    id: '7f6d314f',
+    title: 'housework',
+    date: '2023-02-13T02:32',
+    image: null,
+    important: true,
+    isDone: false,
+    status: TaskStatus.EXPIRED,
+  },
+];
