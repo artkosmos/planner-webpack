17d4eafe5af25b55908b2eafc52a7546ca160036
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2024-06-20 19:07:08 +0300

    update tests fir header component

--- src/components/business/header/header.test.tsx
+++ src/components/business/header/header.test.tsx
@@ -1,90 +1,108 @@
-import { fireEvent, render, within } from '@testing-library/react';
+import { act, fireEvent, within } from '@testing-library/react';
 
-import { renderWithProviders } from '@/__mocks__';
+import { renderWithProviders } from '@/__mocks__/redux-jest-helper';
 import { Header } from '@/components/business/header/header';
+import { tasksActions } from '@/store';
 
 import '@testing-library/jest-dom';
 import '@/__mocks__/match-media-jest';
 
-// jest.mock('react-redux', () => ({
-//   ...jest.requireActual('react-redux'),
-//   useDispatch: jest.fn(),
-// }));
-//
-// const mockedUseDispatch = jest.spyOn(redux, 'useDispatch');
-
 describe('testing of header component', () => {
   test('should render with app name and select with default language', () => {
-    const { getByText, getByTestId } = renderWithProviders(<Header />);
+    const { getByText, getByTestId, getByLabelText } = renderWithProviders(
+      <Header />,
+    );
 
     const header = getByTestId('header');
     const appName = getByText(/Crazy Planner/);
-    const select = getByTestId('select-lang');
+    const filterSelect = getByLabelText('Filter by');
+    const sortSelect = getByLabelText('Sort by');
+    const langSelect = getByLabelText('Language');
 
     expect(header).toContainElement(appName);
-    expect(header).toContainElement(select);
-    expect(select).toHaveTextContent('English');
+    expect(header).toContainElement(langSelect);
+    expect(langSelect).toHaveTextContent('English');
+    expect(header).toContainElement(filterSelect);
+    expect(header).toContainElement(sortSelect);
   });
 
   test('select should contain only supported languages', () => {
-    const numberOfLanguages = 2;
+    const numberOfAppLanguages = 2;
 
-    const { getAllByRole, getByTestId } = renderWithProviders(<Header />);
+    const { getAllByRole, getByLabelText } = renderWithProviders(<Header />);
 
-    const select = getByTestId('select-lang');
-    const selectButton = within(select).getByRole('combobox');
-    fireEvent.mouseDown(selectButton);
-
+    const langSelect = getByLabelText('Language');
+    fireEvent.mouseDown(langSelect);
     const selectOptions = getAllByRole('option');
 
-    expect(selectOptions).toHaveLength(numberOfLanguages);
+    expect(selectOptions).toHaveLength(numberOfAppLanguages);
     expect(selectOptions[0]).toHaveTextContent('English');
     expect(selectOptions[1]).toHaveTextContent('Russian');
   });
 
-  test('should change app language', async () => {
-    const { findByText, getByTestId, getByRole } = renderWithProviders(
-      <Header />,
-    );
-
-    const select = getByTestId('select-lang');
-    const selectButton = within(select).getByRole('combobox');
-    fireEvent.mouseDown(selectButton);
-
-    const lngOption = getByRole('option', { name: 'Russian' });
-    fireEvent.click(lngOption);
-    const appName = await findByText(/Сумасшедший Планировщик/);
-
-    expect(appName).toBeInTheDocument();
-  });
-
   test('should change app theme', async () => {
-    const { getByRole } = renderWithProviders(<Header />);
+    const { getByTestId } = renderWithProviders(<Header />);
+
+    const header = getByTestId('header');
+    const switchTheme = within(header).getByRole('checkbox');
 
     expect(document.body).not.toHaveClass('dark');
 
-    const switchTheme = getByRole('checkbox');
-    fireEvent.click(switchTheme);
-    fireEvent.change(switchTheme, { target: { checked: true } });
+    await act(() => fireEvent.click(switchTheme));
+    await act(() =>
+      fireEvent.change(switchTheme, { target: { checked: true } }),
+    );
 
     expect(document.body).toHaveClass('dark');
   });
 
-  test.skip('should apply sorting to table', async () => {
-    const { getByTestId, getByRole } = render(<Header />);
+  test('should apply sorting to table', async () => {
+    const mockedSortThunk = jest.spyOn(tasksActions, 'setSort');
 
-    const select = getByTestId('select-sort');
-    const selectButton = within(select).getByRole('combobox');
-    fireEvent.mouseDown(selectButton);
+    const { getByLabelText, getByRole, store } = renderWithProviders(
+      <Header />,
+    );
 
-    const sortOption = getByRole('option', { name: 'Importance' });
-    fireEvent.click(sortOption);
+    const sortSelect = getByLabelText('Sort by');
+    fireEvent.mouseDown(sortSelect);
+
+    const sortOption = getByRole('option', { name: 'Date ↑' });
+    await act(() => fireEvent.click(sortOption));
+
+    expect(mockedSortThunk).toHaveBeenCalledWith('date_first');
+    expect(store.getState().tasks.listSort.sortBy).toBe('date_first');
   });
 
-  test.skip('should apply searching to table', async () => {
-    const { getByTestId } = renderWithProviders(<Header />);
+  test('should apply filter to table', async () => {
+    const mockedFilterThunk = jest.spyOn(tasksActions, 'setFilter');
 
-    const searchInput = getByTestId('search-input');
-    fireEvent.change(searchInput, { target: { value: 'mee' } });
+    const { getByLabelText, getByRole, store } = renderWithProviders(
+      <Header />,
+    );
+
+    const filterSelect = getByLabelText('Filter by');
+    fireEvent.mouseDown(filterSelect);
+
+    const filterOption = getByRole('option', { name: 'Important' });
+    await act(() => fireEvent.click(filterOption));
+
+    expect(mockedFilterThunk).toHaveBeenCalledWith('important');
+    expect(store.getState().tasks.listSort.filterBy).toBe('important');
+  });
+
+  test('should change app language', async () => {
+    const { getByRole, getByLabelText } = renderWithProviders(<Header />);
+
+    const langSelect = getByLabelText('Language');
+    fireEvent.mouseDown(langSelect);
+
+    const lngOption = getByRole('option', { name: 'Russian' });
+    await act(() => fireEvent.click(lngOption));
+    const filterSelect = getByLabelText('Фильтровать по');
+    const sortSelect = getByLabelText('Сортировать по');
+
+    expect(filterSelect).toBeInTheDocument();
+    expect(sortSelect).toBeInTheDocument();
+    expect(langSelect).toHaveTextContent('Русский');
   });
 });
